006aacda9ad5ce9b420f7971261cc8782c40bfd7
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-07-18 12:51:00 +0200

    Modify GINPretrainedWithLinearHead

--- src/models/simple_jump_cl/molecule_encoder.py
+++ src/models/simple_jump_cl/molecule_encoder.py
@@ -1,8 +1,10 @@
 """Molecule encoder for the simple jump contrastive model."""
 
-import torch
+
+import dgllife
 import torch.nn as nn
-from molfeat.trans.pretrained import PretrainedDGLTransformer
+from dgl.nn.pytorch.glob import AvgPooling, MaxPooling, SumPooling
+from dgllife.utils import PretrainAtomFeaturizer, PretrainBondFeaturizer, SMILESToBigraph
 
 
 class GINPretrainedWithLinearHead(nn.Module):
@@ -14,24 +16,49 @@ class GINPretrainedWithLinearHead(nn.Module):
         pretrained_name: str = "gin_supervised_infomax",
         out_dim: int = 2048,
         pretrained_dim: int = 300,
+        pooling: str = "mean",
         **kwargs,
     ):
         super().__init__()
         self.pretrained_name = pretrained_name
         self.pretrained_dim = pretrained_dim
         self.out_dim = out_dim
+        self.pooling = pooling
 
-        self.pretrained = PretrainedDGLTransformer(kind=self.pretrained_name, dtype=torch.float32, **kwargs)
+        self.smiles_to_graph = SMILESToBigraph(
+            add_self_loop=False,
+            node_featurizer=PretrainAtomFeaturizer(),
+            edge_featurizer=PretrainBondFeaturizer(),
+            canonical_atom_order=True,
+            explicit_hydrogens=False,
+            num_virtual_nodes=0,
+        )
+
+        self.model = dgllife.model.load_pretrained(self.pretrained_name)
+        self.pooler = self.get_pooling(pooling)
+
         self.head = nn.Linear(self.pretrained_dim, self.out_dim)
 
-        self.device = None
-
     def forward(self, x):
-        x = self.pretrained(x)
-
-        if self.device is None:
-            self.device = x.device
-            self.head = self.head.to(self.device)
-
+        x = self.smiles_to_graph(x)
+        x = self.model(x)
+        x = self.pooler(x)
         x = self.head(x)
         return x
+
+    @staticmethod
+    def get_pooling(pooling: str):
+        """Get pooling method from name.
+
+        Args:
+            pooling: name of the pooling method
+        """
+        pooling = pooling.lower()
+        if pooling in ["mean", "avg", "average"]:
+            return AvgPooling()
+        elif pooling == "sum":
+            return SumPooling()
+        elif pooling == "max":
+            return MaxPooling()
+        else:
+            raise ValueError(f"Pooling: {pooling} not supported !")
