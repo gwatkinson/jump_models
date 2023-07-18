74c710f532475984cc80fd08b3ee70c511f02f25
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-07-18 14:38:22 +0200

    Add pooling and graph batch

--- src/models/simple_jump_cl/molecule_encoder.py
+++ src/models/simple_jump_cl/molecule_encoder.py
@@ -1,10 +1,16 @@
 """Molecule encoder for the simple jump contrastive model."""
 
+import logging
+from typing import List
 
+import dgl
 import dgllife
+import dm
 import torch.nn as nn
 from dgl.nn.pytorch.glob import AvgPooling, MaxPooling, SumPooling
-from dgllife.utils import PretrainAtomFeaturizer, PretrainBondFeaturizer, SMILESToBigraph
+from dgllife.utils import PretrainAtomFeaturizer, PretrainBondFeaturizer, mol_to_bigraph
+
+logger = logging.getLogger(__name__)
 
 
 class GINPretrainedWithLinearHead(nn.Module):
@@ -25,26 +31,17 @@ class GINPretrainedWithLinearHead(nn.Module):
         self.out_dim = out_dim
         self.pooling = pooling
 
-        self.smiles_to_graph = SMILESToBigraph(
-            add_self_loop=False,
-            node_featurizer=PretrainAtomFeaturizer(),
-            edge_featurizer=PretrainBondFeaturizer(),
-            canonical_atom_order=True,
-            explicit_hydrogens=False,
-            num_virtual_nodes=0,
-        )
-
         self.model = dgllife.model.load_pretrained(self.pretrained_name)
         self.pooler = self.get_pooling(pooling)
-
         self.head = nn.Linear(self.pretrained_dim, self.out_dim)
 
     def forward(self, x):
-        x = self.smiles_to_graph(x)
-        x = self.model(x)
-        x = self.pooler(x)
-        x = self.head(x)
-        return x
+        bg = self.graph_featurizer(x)
+        nfeats, efeats = self.get_n_e_feats(bg)
+        node_feats = self.model(bg, nfeats, efeats)
+        z = self.pooler(bg, node_feats)
+        z = self.head(z)
+        return z
 
     @staticmethod
     def get_pooling(pooling: str):
@@ -62,3 +59,34 @@ class GINPretrainedWithLinearHead(nn.Module):
             return MaxPooling()
         else:
             raise ValueError(f"Pooling: {pooling} not supported !")
+
+    @staticmethod
+    def graph_featurizer(smiles: List[str]):
+        graphs = []
+        success = []
+        for smi in smiles:
+            try:
+                mol = dm.to_mol(smi)
+                if mol is None:
+                    success.append(False)
+                    continue
+                g = mol_to_bigraph(
+                    mol,
+                    add_self_loop=True,
+                    node_featurizer=PretrainAtomFeaturizer(),
+                    edge_featurizer=PretrainBondFeaturizer(),
+                    canonical_atom_order=False,
+                )
+                graphs.append(g)
+                success.append(True)
+            except Exception as e:
+                logger.error(e)
+                success.append(False)
+        return dgl.batch(graphs)
+
+    @staticmethod
+    def get_n_e_feats(bg):
+        nfeats = [bg.ndata[k] for k in bg.ndata]
+        efeats = [bg.edata[k] for k in bg.edata]
+
+        return nfeats, efeats
