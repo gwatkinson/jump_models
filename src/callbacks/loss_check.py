44aabef94017803706c5ff858a4d51119f7bda03
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-08-22 16:36:38 +0200

    Fix loss callback

--- src/callbacks/loss_check.py
+++ src/callbacks/loss_check.py
@@ -33,7 +33,6 @@ class LossCheckCallback(Callback):
         self.losses = {
             "train": [],
             "val": [],
-            "test": [],
         }
 
     def on_batch_end(self, phase: str, trainer, pl_module, outputs, batch, batch_idx):
@@ -66,38 +65,33 @@ class LossCheckCallback(Callback):
     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
         self.on_batch_end("val", trainer, pl_module, outputs, batch, batch_idx)
 
-    def on_test_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
-        self.on_batch_end("test", trainer, pl_module, outputs, batch, batch_idx)
-
     @staticmethod
     def get_loss_from_list_of_dict(losses: List[Dict[str, Any]]):
         return [loss["loss"] for loss in losses]
 
     @staticmethod
     def get_loss_by_epoch(losses: List[Dict[str, Any]]):
+        py_logger.info(f"Losses dict: {losses}")
+        py_logger.info(pd.DataFrame(losses))
+
         return pd.DataFrame(losses).groupby("epoch")["loss"].sum().reset_index()
 
     def on_fit_end(self, trainer, pl_module):
         train_step_loss = self.get_loss_from_list_of_dict(self.losses["train"])
         val_step_loss = self.get_loss_from_list_of_dict(self.losses["val"])
-        test_step_loss = self.get_loss_from_list_of_dict(self.losses["test"])
 
         py_logger.info(f"Train step loss: {train_step_loss}")
         py_logger.info(f"Val step loss: {val_step_loss}")
-        py_logger.info(f"Test step loss: {test_step_loss}")
 
         train_epoch_loss = self.get_loss_by_epoch(self.losses["train"])
         val_epoch_loss = self.get_loss_by_epoch(self.losses["val"])
-        test_epoch_loss = self.get_loss_by_epoch(self.losses["test"])
 
         py_logger.info(f"Train epoch loss: {train_epoch_loss}")
         py_logger.info(f"Val epoch loss: {val_epoch_loss}")
-        py_logger.info(f"Test epoch loss: {test_epoch_loss}")
 
         fig, ax = plt.subplots()
         ax.plot(train_epoch_loss["epoch"], train_epoch_loss["loss"], label="train")
         ax.plot(val_epoch_loss["epoch"], val_epoch_loss["loss"], label="val")
-        ax.plot(test_epoch_loss["epoch"], test_epoch_loss["loss"], label="test")
         ax.legend()
         ax.set_xlabel("Epoch")
         ax.set_ylabel("Loss")
--- src/modules/transforms/__init__.py
+++ src/modules/transforms/__init__.py
@@ -1,4 +1,8 @@
 # flake8: noqa: F401
 
+import torchvision
+
 from src.modules.transforms.default import DefaultJUMPTransform
 from src.modules.transforms.image_normalization import ImageNormalization
+
+torchvision.disable_beta_transforms_warning()
