a3c7419a347b6c40c0ddf314a9558bb622a5287f
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-08-24 17:12:00 +0200

    Try to fix default collate function

--- src/modules/collate_fn/default_collate.py
+++ src/modules/collate_fn/default_collate.py
@@ -1,10 +1,11 @@
-from dgl import DGLHeteroGraph, batch
+from dgl import DGLGraph
+from dgl import batch as dgl_batch
 from torch.utils.data._utils.collate import collate, default_collate_fn_map
 
 default_collate_fn_map[
-    DGLHeteroGraph
-] = batch  # Add DGLGraph to default_collate_fn_map of torch.utils.data._utils.collate
+    DGLGraph
+] = dgl_batch  # Add DGLGraph to default_collate_fn_map of torch.utils.data._utils.collate
 
 
-def default_collate(batch):
-    return collate(batch, collate_fn_map=default_collate_fn_map)
+def default_collate(x):
+    return collate(x, collate_fn_map=default_collate_fn_map)
