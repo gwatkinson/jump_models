ac5f73edf25815c0800104699bc99e747f48b3b1
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-08-24 17:20:21 +0200

    Try to fix default collate function

--- src/modules/collate_fn/default_collate.py
+++ src/modules/collate_fn/default_collate.py
@@ -1,11 +1,19 @@
+from typing import Callable, Dict, Optional, Tuple, Type, Union
+
 from dgl import DGLGraph
 from dgl import batch as dgl_batch
-from torch.utils.data._utils.collate import collate, default_collate_fn_map
+from torch.utils.data._utils.collate import collate
+from torch.utils.data._utils.collate import default_collate_fn_map as _default_collate_fn_map
 
-default_collate_fn_map[
+
+def collate_dgl_graph_fn(batch, *, collate_fn_map: Optional[Dict[Union[Type, Tuple[Type, ...]], Callable]] = None):
+    return dgl_batch(batch)
+
+
+_default_collate_fn_map[
     DGLGraph
-] = dgl_batch  # Add DGLGraph to default_collate_fn_map of torch.utils.data._utils.collate
+] = collate_dgl_graph_fn  # Add DGLGraph to default_collate_fn_map of torch.utils.data._utils.collate
 
 
 def default_collate(x):
-    return collate(x, collate_fn_map=default_collate_fn_map)
+    return collate(x, collate_fn_map=_default_collate_fn_map)
