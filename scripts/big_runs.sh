5dfcbd745f1d8a3e1c3a1946f09668b1381bc58d
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-08-27 14:51:31 +0200

    Change patience for reduce lr on plateau and early stopping

--- scripts/big_runs.sh
+++ scripts/big_runs.sh
@@ -2,14 +2,27 @@
 
 git pull
 
-python src/train.py experiment=gin_context_pred/big trainer=gpu trainer.devices=[1] data.num_workers=32 seed=23540 model/criterion=info_nce logger.wandb.project=first_real_runs logger.wandb.group=data_size
+python src/train.py \
+    experiment=gin_context_pred/big \
+    trainer=gpu trainer.devices=[1] \
+    data.num_workers=32 \
+    seed=23540 \
+    model/criterion=info_nce \
+    logger.wandb.project=big_runs logger.wandb.group=gin_context_pred
 
-python src/train.py experiment=fp_big/big trainer=gpu trainer.devices=[1] data.num_workers=32 seed=23540 model/criterion=info_nce logger.wandb.project=first_real_runs logger.wandb.group=data_size
+python src/train.py \
+    experiment=fp_big \
+    trainer=gpu trainer.devices=[1] \
+    data.num_workers=32 \
+    seed=2131 \
+    model/criterion=info_nce \
+    logger.wandb.project=big_runs logger.wandb.group=fingerprints
 
-python src/train.py experiment=gin_context_pred/med trainer=gpu trainer.devices=[1] data.num_workers=64 seed=23540 model/criterion=info_nce_reg logger.wandb.project=first_real_runs logger.wandb.group=info_nce
-python src/train.py experiment=gin_context_pred/med trainer=gpu trainer.devices=[1] data.num_workers=64 seed=23540 model/criterion=info_nce_temp logger.wandb.project=first_real_runs logger.wandb.group=info_nce
-python src/train.py experiment=gin_context_pred/med trainer=gpu trainer.devices=[1] data.num_workers=64 seed=23540 model/criterion=info_nce_reg model.criterion.temperature_requires_grad=True logger.wandb.project=first_real_runs logger.wandb.group=info_nce
+python src/train.py \
+    experiment=gin_context_pred/big \
+    trainer=gpu trainer.devices=[1] \
+    data.num_workers=32 \
+    seed=23540 \
+    model/criterion=ntxent_reg model.criterion.alpha=0.1 \
+    logger.wandb.project=big_runs logger.wandb.group=gin_context_pred
 
-python src/train.py experiment=gin_context_pred/med trainer=gpu trainer.devices=[1] data.num_workers=64 seed=23540 model/criterion=ntxent logger.wandb.project=first_real_runs logger.wandb.group=ntxent
-python src/train.py experiment=gin_context_pred/med trainer=gpu trainer.devices=[1] data.num_workers=64 seed=23540 model/criterion=ntxent_reg logger.wandb.project=first_real_runs logger.wandb.group=ntxent
-python src/train.py experiment=gin_context_pred/med trainer=gpu trainer.devices=[1] data.num_workers=64 seed=23540 model/criterion=ntxent_reg model.criterion.temperature_requires_grad=True logger.wandb.project=first_real_runs logger.wandb.group=ntxent
