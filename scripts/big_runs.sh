da979ea3223c9489439b428168f79945039622a7
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-08-29 10:50:09 +0200

    Reparametrize the runs

--- scripts/big_runs.sh
+++ scripts/big_runs.sh
@@ -4,19 +4,38 @@ git pull
 
 python src/train.py \
     experiment=gin_context_pred/big \
-    trainer=gpu trainer.devices=[1] trainer.max_epochs=200 \
-    data.num_workers=32 \
     seed=23540 \
+    trainer=gpu \
+    trainer.devices=[1] \
+    trainer.max_epochs=200 \
+    data.num_workers=32 \
     model/criterion=info_nce \
-    logger.wandb.project=first_real_runs logger.wandb.group=big_runs
+    logger.wandb.project=first_real_runs \
+    logger.wandb.group=big_runs
+
+python src/train.py \
+    experiment=gin_context_pred/big \
+    seed=23540 \
+    trainer=gpu \
+    trainer.devices=[1] \
+    trainer.max_epochs=200 \
+    data.num_workers=32 \
+    model.lr=5e-4 \
+    model/criterion=info_nce \
+    logger.wandb.project=first_real_runs \
+    logger.wandb.group=big_runs
+
 
 python src/train.py \
     experiment=fp_big \
-    trainer=gpu trainer.devices=[1] trainer.max_epochs=100 \
-    data.num_workers=32 \
     seed=2131 \
+    trainer=gpu \
+    trainer.devices=[1] \
+    trainer.max_epochs=100 \
+    data.num_workers=32 \
     model/criterion=info_nce \
-    logger.wandb.project=first_real_runs logger.wandb.group=big_runs
+    logger.wandb.project=first_real_runs \
+    logger.wandb.group=big_runs
 
 python src/train.py \
     experiment=gin_context_pred/big \
@@ -28,5 +47,6 @@ python src/train.py \
     model.criterion.mse_reg=0.5 \
     model.criterion.variance_reg=1 \
     model.criterion.covariance_reg=0.25 \
-    logger.wandb.project=first_real_runs logger.wandb.group=big_runs
+    logger.wandb.project=first_real_runs \
+    logger.wandb.group=big_runs
 
