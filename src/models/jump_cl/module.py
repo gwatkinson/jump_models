8ff1536d199a80d9ad30fa949aed18ebd2a395b8
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-08-31 11:09:58 +0200

    Add rank to loss

--- src/models/jump_cl/module.py
+++ src/models/jump_cl/module.py
@@ -11,38 +11,6 @@ from src.utils import pylogger
 logger = pylogger.get_pylogger(__name__)
 
 
-# def info_nce_loss(self, batch, mode="train"):
-#     imgs, _ = batch
-#     imgs = torch.cat(imgs, dim=0)
-
-#     # Encode all images
-#     feats = self.convnet(imgs)
-#     # Calculate cosine similarity
-#     cos_sim = F.cosine_similarity(feats[:, None, :], feats[None, :, :], dim=-1)
-#     # Mask out cosine similarity to itself
-#     self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=cos_sim.device)
-#     cos_sim.masked_fill_(self_mask, -9e15)
-#     # Find positive example -> batch_size//2 away from the original example
-#     pos_mask = self_mask.roll(shifts=cos_sim.shape[0] // 2, dims=0)
-#     # InfoNCE loss
-#     cos_sim = cos_sim / self.hparams.temperature
-#     nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
-#     nll = nll.mean()
-
-#     # Logging loss
-#     self.log(mode + "_loss", nll)
-#     # Get ranking position of positive example
-#     comb_sim = torch.cat(
-#         [cos_sim[pos_mask][:, None], cos_sim.masked_fill(pos_mask, -9e15)],  # First position positive example
-#         dim=-1,
-#     )
-#     sim_argsort = comb_sim.argsort(dim=-1, descending=True).argmin(dim=-1)
-#     # Logging ranking metrics
-#     self.log(mode + "_acc_top1", (sim_argsort == 0).float().mean())
-#     self.log(mode + "_acc_top5", (sim_argsort < 5).float().mean())
-#     self.log(mode + "_acc_mean_pos", 1 + sim_argsort.float().mean())
-
-
 class BasicJUMPModule(LightningModule):
     """Basic Jump LightningModule to run a simple contrastive training.
 
--- src/modules/losses/contrastive_losses.py
+++ src/modules/losses/contrastive_losses.py
@@ -7,11 +7,42 @@ from torch import Tensor
 from src.modules.losses.base_losses import LossWithTemperature, RegWithTemperatureLoss
 
 
+def calculate_rank(sim_matrix: Tensor) -> Tensor:
+    with torch.no_grad():
+        labels = torch.arange(sim_matrix.shape[0], device=sim_matrix.device).repeat(sim_matrix.shape[0], 1).t()
+
+        # X to Y ranking
+        row_order_x = sim_matrix.argsort(descending=True, dim=-1)
+        x_to_y = (row_order_x == labels).nonzero(as_tuple=True)[1]
+
+        # Y to X ranking
+        row_order_y = sim_matrix.argsort(descending=True, dim=0).t()
+        y_to_x = (row_order_y == labels).nonzero(as_tuple=True)[1]
+
+        batch_size = sim_matrix.shape[0]
+
+        results = {
+            "x_to_y_top1": (x_to_y == 0).float().mean(),
+            "x_to_y_top5": (x_to_y < 5).float().mean(),
+            "x_to_y_top10": (x_to_y < 10).float().mean(),
+            "x_to_y_mean_pos": 1 + x_to_y.float().mean(),
+            "x_to_y_mean_pos_normed": (1 + x_to_y.float().mean()) / batch_size,
+            "y_to_x_top1": (y_to_x == 0).float().mean(),
+            "y_to_x_top5": (y_to_x < 5).float().mean(),
+            "y_to_x_top10": (y_to_x < 10).float().mean(),
+            "y_to_x_mean_pos": 1 + y_to_x.float().mean(),
+            "y_to_x_mean_pos_normed": (1 + y_to_x.float().mean()) / batch_size,
+        }
+
+    return results
+
+
 class InfoNCE(LossWithTemperature):
     def __init__(
         self,
         norm: bool = True,
         temperature: float = 0.5,
+        return_rank: bool = False,
         eps: float = 1e-8,
         name: Optional[str] = None,
         **kwargs,
@@ -21,6 +52,7 @@ class InfoNCE(LossWithTemperature):
         self.norm = norm
         self.eps = eps
         self.name = name or "InfoNCE"
+        self.return_rank = return_rank
 
     def forward(self, z1, z2, **kwargs) -> Tensor:
         batch_size, _ = z1.size()
@@ -36,6 +68,10 @@ class InfoNCE(LossWithTemperature):
         loss = pos_sim / sim_matrix.sum(dim=1)
         loss = -torch.log(loss).mean()
 
+        if self.return_rank:
+            results = calculate_rank(sim_matrix)
+            return {"loss": loss, **results}
+
         return {"loss": loss}
 
 
@@ -49,6 +85,7 @@ class NTXent(LossWithTemperature):
         self,
         norm: bool = True,
         temperature: float = 0.5,
+        return_rank: bool = False,
         eps: float = 1e-8,
         name: Optional[str] = None,
         **kwargs,
@@ -57,6 +94,7 @@ class NTXent(LossWithTemperature):
         self.norm = norm
         self.eps = eps
         self.name = name or "NTXent"
+        self.return_rank = return_rank
 
     def forward(self, z1, z2, **kwargs) -> Tensor:
         batch_size, _ = z1.size()
@@ -71,6 +109,10 @@ class NTXent(LossWithTemperature):
         pos_sim = torch.diagonal(sim_matrix)
         loss = pos_sim / (sim_matrix.sum(dim=1) - pos_sim)  # This is the difference from InfoNCE
         loss = -torch.log(loss).mean()
+
+        if self.return_rank:
+            results = calculate_rank(sim_matrix)
+            return {"loss": loss, **results}
 
         return {"loss": loss}
 
