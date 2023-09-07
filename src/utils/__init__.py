281222516c2c4da12becce0804fa90ddcfbf405b
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-09-07 17:39:23 +0200

    Change big config

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,10 @@
 # flake8: noqa: F401
-from src.utils.instantiators import instantiate_callbacks, instantiate_evaluator_list, instantiate_loggers
+from src.utils.instantiators import (
+    instantiate_callbacks,
+    instantiate_evaluator,
+    instantiate_evaluator_list,
+    instantiate_loggers,
+)
 from src.utils.logging_utils import log_ckpt_path, log_hyperparameters
 from src.utils.pylogger import get_pylogger
 from src.utils.rich_utils import enforce_tags, print_config_tree
