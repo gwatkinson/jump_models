6a3d9178d07def24e804eae52890b3b1fa25be4e
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-09-15 14:01:56 +0200

    Add random split experiment

--- src/splitters/random.py
+++ src/splitters/random.py
@@ -3,32 +3,53 @@ from typing import Dict, List
 from sklearn.model_selection import train_test_split
 
 from src.splitters import BaseSplitter
+from src.utils import pylogger
+
+py_logger = pylogger.get_pylogger(__name__)
 
 
 class RandomSplitter(BaseSplitter):
-    """Split data randomly into train, val and test sets."""
+    """Split the data into train, val and test sets using the scaffold
+    splitting method."""
 
     def split(self) -> Dict[str, List[str]]:
         """Split the data into train, val and test sets."""
         if self.compound_list is None:
             raise ValueError("Compound list is None.")
 
-        if self.input_type == "int":
-            train_val, test = train_test_split(
-                self.compound_list,
-                test_size=self.test,
-                train_size=self.train + self.val,
-                random_state=self.random_state,
-            )
-            train, val = train_test_split(
-                train_val, test_size=self.val, train_size=self.train, random_state=self.random_state
-            )
-        elif self.normalized:
-            train_val, test = train_test_split(self.compound_list, test_size=self.test, random_state=self.random_state)
-            train, val = train_test_split(
-                train_val, test_size=self.val / (self.train + self.val), random_state=self.random_state
-            )
-        else:
-            raise ValueError("Error in the code.")
+        py_logger.info("Generating sets ...")
 
-        return {"train": train, "val": val, "test": test}
+        train_val_retrieval, test_cpds = train_test_split(
+            self.compound_list,
+            test_size=self.test,
+            random_state=self.random_state,
+        )
+        train_val, retrieval_cpds = train_test_split(
+            train_val_retrieval, test_size=self.retrieval, random_state=self.random_state
+        )
+        total_train_cpds, val_cpds = train_test_split(train_val, test_size=self.val, random_state=self.random_state)
+
+        output = {
+            "total_train": total_train_cpds,
+            "val": val_cpds,
+            "test": test_cpds,
+            "retrieval": retrieval_cpds,
+        }
+
+        self.total_train_cpds = total_train_cpds
+        self.val_cpds = val_cpds
+        self.test_cpds = test_cpds
+        self.retrieval_cpds = retrieval_cpds
+
+        return output
+
+    def split_train(self, total_train_cpds=None) -> List[str]:
+        total_train_cpds = total_train_cpds or self.total_train_cpds
+
+        train_cpds, _ = train_test_split(
+            total_train_cpds,
+            train_size=self.train,
+            random_state=self.random_state,
+        )
+        self.train_cpds = train_cpds
+        return train_cpds
