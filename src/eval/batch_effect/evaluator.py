94fea801cf19115f4db0392a758aff760e4839ce
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-09-18 17:59:06 +0200

    Remove bandit

--- src/eval/batch_effect/evaluator.py
+++ src/eval/batch_effect/evaluator.py
@@ -1,5 +1,6 @@
-import io
+import json
 import os.path as osp
+import pickle
 from pathlib import Path
 from typing import Optional
 
@@ -26,6 +27,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 
 from src.eval import Evaluator
+from src.eval.batch_effect.spherize import ZCA_corr
 
 # from src.utils import pylogger
 
@@ -62,6 +64,7 @@ class BatchEffectEvaluator(Evaluator):
         source_split: bool = True,
         well_split: bool = True,
         fully_random_split: bool = True,
+        dmso_normalize: bool = True,
         out_dir: Optional[str] = None,
         name: Optional[str] = None,
         visualize_kwargs: Optional[dict] = None,
@@ -71,6 +74,9 @@ class BatchEffectEvaluator(Evaluator):
         self.trainer = trainer
 
         self.embeddings_df: Optional[pd.DataFrame] = None
+
+        self.dmso_normalize = dmso_normalize
+        self.dmso_embeddings: Optional[dict] = None
 
         self.plot = plot
         self.logistic = logistic
@@ -111,34 +117,77 @@ class BatchEffectEvaluator(Evaluator):
     def visualize(self, outs, **kwargs):
         pass
 
-    def get_embeddings(self):
-        if self.embeddings_df is not None:
-            print("Found embeddings_df, skipping getting embeddings...")
-            self.n_labels = self.embeddings_df["label"].nunique()
-            self.label_encoder = LabelEncoder()
-            self.label_encoder.fit(self.embeddings_df["label"])
+    def get_dmso_embeddings(self):
+        if self.dmso_embeddings is not None:
+            print("Found dmso_embeddings, skipping getting embeddings...")
             return
 
-        embedding_path = f"{self.out_dir}/embeddings.parquet" if self.out_dir is not None else None
+        embedding_path = f"{self.out_dir}/dmso_embeddings.pickle" if self.out_dir is not None else None
         if embedding_path and Path(embedding_path).exists():
-            print(f'Found embeddings at "{embedding_path}", skipping getting embeddings...')
-            self.embeddings_df = pd.read_parquet(embedding_path)
-            self.n_labels = self.embeddings_df["label"].nunique()
-            self.label_encoder = LabelEncoder()
-            self.label_encoder.fit(self.embeddings_df["label"])
+            print(f'Found dmso_df at "{embedding_path}", skipping getting embeddings...')
+            with open(embedding_path, "rb") as f:
+                self.dmso_embeddings = pickle.load(f)
             return
 
-        predictions = self.trainer.predict(self.model, self.datamodule)
+        predictions = self.trainer.predict(self.model, self.datamodule.dmso_dataloader())
         keys = list(predictions[0].keys())
 
-        self.embeddings_df = pd.DataFrame({key: concat_from_list_of_dict(predictions, key) for key in keys})
+        dmso_embeddings_df = pd.DataFrame({key: concat_from_list_of_dict(predictions, key) for key in keys})
+
+        all_batches = dmso_embeddings_df["batch"].unique()
+
+        transform_dict = {}
+        for batchi in all_batches:
+            dmso_embeddings_batch = np.array(dmso_embeddings_df.query("batch==@batchi").embeddings.to_list())
+            spherizer = ZCA_corr()
+            spherizer.fit(dmso_embeddings_batch)
+            transform_dict[batchi] = spherizer
+
+        self.dmso_embeddings = transform_dict
+
+        if embedding_path:
+            with open(embedding_path, "wb") as f:
+                pickle.dump(transform_dict, f)
+
+    def get_embeddings(self):
+        if self.dmso_normalize:
+            self.get_dmso_embeddings()
+
+        embedding_path = f"{self.out_dir}/embeddings.parquet" if self.out_dir is not None else None
+
+        if self.embeddings_df is not None:
+            print("Found embeddings_df, skipping getting embeddings...")
+
+        elif embedding_path and Path(embedding_path).exists():
+            print(f'Found embeddings at "{embedding_path}", skipping getting embeddings...')
+            self.embeddings_df = pd.read_parquet(embedding_path)
+        else:
+            predictions = self.trainer.predict(self.model, self.datamodule)
+            keys = list(predictions[0].keys())
+
+            self.embeddings_df = pd.DataFrame({key: concat_from_list_of_dict(predictions, key) for key in keys})
+
+            self.n_labels = self.embeddings_df["label"].nunique()
+            self.label_encoder = LabelEncoder()
+            self.label_encoder.fit(self.embeddings_df["label"])
+
+            if embedding_path:
+                self.embeddings_df.to_parquet(embedding_path, index=False)
+
+        if self.dmso_normalize and self.dmso_embeddings:
+            for batchi in self.embeddings_df["batch"].unique():
+                try:
+                    dmso_embeddings_batch = np.array(self.embeddings_df.query("batch==@batchi").embeddings.to_list())
+                    spherizer = self.dmso_embeddings[batchi]
+                    self.embeddings_df.loc[self.embeddings_df.batch == batchi, "embedding"] = spherizer.transform(
+                        dmso_embeddings_batch
+                    ).tolist()
+                except Exception as e:
+                    print(f"Error while normalizing batch {batchi}: {e}")
 
         self.n_labels = self.embeddings_df["label"].nunique()
         self.label_encoder = LabelEncoder()
         self.label_encoder.fit(self.embeddings_df["label"])
-
-        if embedding_path:
-            self.embeddings_df.to_parquet(embedding_path, index=False)
 
     def plot_tsne(self, embeddings, col, title="t-SNE"):
         try:
@@ -149,11 +198,9 @@ class BatchEffectEvaluator(Evaluator):
 
             if self.out_dir:
                 print(f"Saved {title} to {self.out_dir}/{title.replace(' ', '_')}.png")
-                fig.savefig(osp.join(self.out_dir, f"/{title.replace(' ', '_')}.png"))
-            # emb_buf = io.BytesIO()
-            # fig.savefig(emb_buf)
-            # plt.close(fig)
+                fig.savefig(osp.join(self.out_dir, f"{title.replace(' ', '_')}.png"))
             return fig
+
         except Exception as e:
             print(f"Error while plotting t-SNE: {e}")
 
@@ -162,34 +209,24 @@ class BatchEffectEvaluator(Evaluator):
         tsne = TSNE(n_components=2, random_state=0)
         embeddings = tsne.fit_transform(np.array(self.embeddings_df["embedding"].tolist()))
 
-        # images = []
-
-        # Plot with regards to label
-        self.plot_tsne(embeddings, "label", "t-SNE colored by labels")
-        # images.append()
-
-        # Plot with regards to batch
-        # images.append(
-        self.plot_tsne(embeddings, "batch", "t-SNE colored by batch")
-
-        # Plot with regards to plate
-        # images.append(
-        self.plot_tsne(embeddings, "plate", "t-SNE colored by plate")
-
-        # Plot with regards to well
-        # images.append(
-        self.plot_tsne(embeddings, "well", "t-SNE colored by well")
-
-        # Plot with regards to source
-        # images.append(
-        self.plot_tsne(embeddings, "source", "t-SNE colored by source")
+        images = [
+            self.plot_tsne(embeddings, "label", "t-SNE colored by labels"),
+            self.plot_tsne(embeddings, "batch", "t-SNE colored by batch"),
+            self.plot_tsne(embeddings, "plate", "t-SNE colored by plate"),
+            self.plot_tsne(embeddings, "well", "t-SNE colored by well"),
+            self.plot_tsne(embeddings, "source", "t-SNE colored by source"),
+        ]
 
         # Log plots to WandB
-        # if self.logger:
-        #     self.logger.log_image(key=key, images=images)
-        #     self.logger.save()
+        try:
+            if self.logger:
+                # wandb_images = [wandb.Image(image) for image in images]
+                self.logger.log_image(key=key, images=images)
+                self.logger.save()
+        except Exception as e:
+            print(f"Error while logging t-SNE plots: {e}")
 
-    def plot_results(self, cls, X_test, y_test, key):
+    def get_metric_dict(self, cls, X_test, y_test, key):
         # Metrics
         labels = np.arange(len(self.label_encoder.classes_))
         self.scorers = {
@@ -199,122 +236,136 @@ class BatchEffectEvaluator(Evaluator):
             "recall": make_scorer(recall_score, average="macro", labels=labels),
             "f1": make_scorer(f1_score, average="macro", labels=labels),
             "roc_auc": make_scorer(roc_auc_score, average="macro", multi_class="ovr", needs_proba=True, labels=labels),
-            "top_3": make_scorer(top_k_accuracy_score, k=3, labels=labels),
-            "top_5": make_scorer(top_k_accuracy_score, k=5, labels=labels),
-            "top_10": make_scorer(top_k_accuracy_score, k=10, labels=labels),
+            "top_3": make_scorer(top_k_accuracy_score, k=3, labels=labels, needs_proba=True),
+            "top_5": make_scorer(top_k_accuracy_score, k=5, labels=labels, needs_proba=True),
+            "top_10": make_scorer(top_k_accuracy_score, k=10, labels=labels, needs_proba=True),
         }
 
         metric_dict = {}
         for k, scorer in self.scorers.items():
             try:
-                metric_dict[k] = scorer(cls, X_test, y_test)
+                metric_dict[f"{key}/{k}"] = scorer(cls, X_test, y_test)
             except Exception as e:
                 print(f"Error while computing {k}: {e}")
 
-        # metric_dict = {k: scorer(cls, X_test, y_test) for k, scorer in self.scorers.items()}
+        if self.out_dir:
+            print(f"Saved metrics to {self.out_dir}/{key}_metrics.json")
+            with open(f"{self.out_dir}/{key}_metrics.json", "w") as f:
+                json.dump(metric_dict, f)
 
-        images = []
+        return metric_dict
 
-        y_probas = cls.predict_proba(X_test)
-        y_test_str = self.label_encoder.inverse_transform(y_test)
-        y_predict_str = self.label_encoder.inverse_transform(cls.predict(X_test))
-        labels = self.label_encoder.classes_
-
-        # Plot confusion matrix
+    def plot_conf_matrix(self, cls, X_test, y_test, key):
         try:
-            fig, ax = plt.subplots(figsize=(14, 14))
             labels = self.label_encoder.classes_
-            plot_confusion_matrix(y_test_str, y_predict_str, labels=labels, ax=ax, x_tick_rotation=90)
-            conf_buf = io.BytesIO()
-            fig.savefig(conf_buf)
-            plt.close(fig)
-            images.append(conf_buf)
+            fig, ax = plt.subplots(figsize=(14, 14))
+            plot_confusion_matrix(
+                self.label_encoder.inverse_transform(y_test),
+                self.label_encoder.inverse_transform(cls.predict(X_test)),
+                labels=labels,
+                ax=ax,
+                x_tick_rotation=90,
+                title=(title := f"{key}/Confusion matrix"),
+            )
+            fig.savefig(f"{self.out_dir}/{title.replace(' ', '_')}.png")
+            return fig
         except Exception as e:
             print(f"Error while plotting confusion matrix: {e}")
 
-        # Plot ROC curve
+    def plot_macro_roc_curve(self, cls, X_test, y_test, key):
         try:
             fig, ax = plt.subplots(figsize=(14, 14))
             plot_roc(
-                y_test_str,
-                y_probas,
+                self.label_encoder.inverse_transform(y_test),
+                cls.predict_proba(X_test),
                 ax=ax,
                 plot_macro=True,
                 plot_micro=False,
                 classes_to_plot=[],
-                title=(title := "Macro-average ROC curve"),
+                title=(title := f"{key}/Macro-average ROC curve"),
             )
             fig.savefig(f"{self.out_dir}/{title.replace(' ', '_')}.png")
-            roc_buf = io.BytesIO()
-            fig.savefig(roc_buf)
-            plt.close(fig)
-            images.append(roc_buf)
+            return fig
         except Exception as e:
-            print(f"Error while plotting ROC curve: {e}")
+            print(f"Error while plotting macro-average ROC curve: {e}")
 
-        # Plot total ROC curve
+    def plot_roc_curves(self, cls, X_test, y_test, key):
         try:
             fig, ax = plt.subplots(figsize=(14, 14))
             plot_roc(
-                y_test_str,
-                y_probas,
+                self.label_encoder.inverse_transform(y_test),
+                cls.predict_proba(X_test),
                 ax=ax,
                 plot_macro=True,
                 plot_micro=True,
                 classes_to_plot=None,
-                title=(title := "ROC curves"),
+                title=(title := f"{key}/ROC curves"),
             )
             fig.savefig(f"{self.out_dir}/{title.replace(' ', '_')}.png")
-            tot_roc_buf = io.BytesIO()
-            fig.savefig(tot_roc_buf)
-            plt.close(fig)
-            images.append(tot_roc_buf)
+            return fig
         except Exception as e:
-            print(f"Error while plotting total ROC curve: {e}")
+            print(f"Error while plotting ROC curves: {e}")
 
-        # Plot precision-recall curve
+    def plot_micro_pr_curve(self, cls, X_test, y_test, key):
         try:
             fig, ax = plt.subplots(figsize=(14, 14))
             plot_precision_recall(
-                y_test_str,
-                y_probas,
+                self.label_encoder.inverse_transform(y_test),
+                cls.predict_proba(X_test),
                 ax=ax,
                 plot_micro=True,
                 classes_to_plot=[],
-                title=(title := "Micro-average precision-recall curve"),
+                title=(title := f"{key}/Micro-average precision-recall curve"),
             )
             fig.savefig(f"{self.out_dir}/{title.replace(' ', '_')}.png")
-            pr_buf = io.BytesIO()
-            fig.savefig(pr_buf)
-            plt.close(fig)
-            images.append(pr_buf)
+            return fig
         except Exception as e:
-            print(f"Error while plotting precision-recall curve: {e}")
+            print(f"Error while plotting micro-average precision-recall curve: {e}")
 
-        # Plot total precision-recall curve
+    def plot_pr_curves(self, cls, X_test, y_test, key):
         try:
             fig, ax = plt.subplots(figsize=(14, 14))
             plot_precision_recall(
-                y_test_str,
-                y_probas,
+                self.label_encoder.inverse_transform(y_test),
+                cls.predict_proba(X_test),
                 ax=ax,
                 plot_micro=True,
                 classes_to_plot=None,
-                title=(title := "Precision-recall curves"),
+                title=(title := f"{key}/Precision-recall curves"),
             )
             fig.savefig(f"{self.out_dir}/{title.replace(' ', '_')}.png")
-            tot_pr_buf = io.BytesIO()
-            fig.savefig(tot_pr_buf)
-            plt.close(fig)
-            images.append(tot_pr_buf)
+            return fig
         except Exception as e:
-            print(f"Error while plotting total precision-recall curve: {e}")
+            print(f"Error while plotting precision-recall curves: {e}")
+
+    def plot_metrics(self, cls, X_test, y_test, key):
+        images = []
+
+        for func in [
+            self.plot_conf_matrix,
+            self.plot_macro_roc_curve,
+            self.plot_roc_curves,
+            self.plot_micro_pr_curve,
+            self.plot_pr_curves,
+        ]:
+            fig = func(cls, X_test, y_test, key)
+            if fig:
+                images.append(fig)
+
+        return images
+
+    def plot_results(self, cls, X_test, y_test, key):
+        metric_dict = self.get_metric_dict(cls, X_test, y_test, key)
+        metric_images = self.plot_metrics(cls, X_test, y_test, key)
 
         # Log to WandB
-        if self.logger:
-            self.logger.log_metrics(metric_dict)
-            # self.logger.log_image(key=key, images=images)
-            self.logger.save()
+        try:
+            if self.logger:
+                self.logger.log_metrics(metric_dict)
+                self.logger.log_image(key=key, images=metric_images)
+                self.logger.save()
+        except Exception as e:
+            print(f"Error while logging metrics: {e}")
 
     def not_same_well_cls(self, cls, key="batch_effect/NotSameWell"):
         wells = self.embeddings_df["well"].unique()
@@ -427,25 +478,27 @@ class BatchEffectEvaluator(Evaluator):
             print("Plotting the embeddings...")
             self.plot_embeddings()
 
+        key_prefix = "batch_effect/regular" if not self.dmso_normalize else "batch_effect/dmso_normalized"
+
         if self.logistic:
             print("Running Logistic Regressions...")
             cls = LogisticRegression(max_iter=1000)
 
             if self.fully_random_split:
                 print("Fully random split")
-                self.fully_random(cls)
+                self.fully_random(cls, key=f"{key_prefix}/LR/FullyRandom")
             if self.well_split:
                 print("Not same well")
-                self.not_same_well_cls(cls)
+                self.not_same_well_cls(cls, key=f"{key_prefix}/LR/NotSameWell")
             if self.batch_split:
                 print("Not same batch")
-                self.not_same_batch(cls)
+                self.not_same_batch(cls, key=f"{key_prefix}/LR/NotSameBatch")
             if self.plate_split:
                 print("Not same plate")
-                self.not_same_plate(cls)
+                self.not_same_plate(cls, key=f"{key_prefix}/LR/NotSamePlate")
             if self.source_split:
                 print("Not same source")
-                self.not_same_source(cls)
+                self.not_same_source(cls, key=f"{key_prefix}/LR/NotSameSource")
 
         if self.knn:
             print("Running KNN Classifier...")
@@ -453,16 +506,16 @@ class BatchEffectEvaluator(Evaluator):
 
             if self.fully_random_split:
                 print("Fully random split")
-                self.fully_random(cls)
+                self.fully_random(cls, key=f"{key_prefix}/KNN/FullyRandom")
             if self.well_split:
                 print("Not same well")
-                self.not_same_well_cls(cls)
+                self.not_same_well_cls(cls, key=f"{key_prefix}/KNN/NotSameWell")
             if self.batch_split:
                 print("Not same batch")
-                self.not_same_batch(cls)
+                self.not_same_batch(cls, key=f"{key_prefix}/KNN/NotSameBatch")
             if self.plate_split:
                 print("Not same plate")
-                self.not_same_plate(cls)
+                self.not_same_plate(cls, key=f"{key_prefix}/KNN/NotSamePlate")
             if self.source_split:
                 print("Not same source")
-                self.not_same_source(cls)
+                self.not_same_source(cls, key=f"{key_prefix}/KNN/NotSameSource")
