6b7ac2e424398cf75620efdb5e01dbc5c3cd1689
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-09-26 15:48:21 +0200

    Update Hint config

--- src/callbacks/wandb.py
+++ src/callbacks/wandb.py
@@ -9,10 +9,7 @@ import seaborn as sns
 from lightning.pytorch.callbacks import Callback
 from lightning.pytorch.loggers import WandbLogger
 
-import wandb
 from src.utils import pylogger
-
-# from src.utils.visualisation import pp_matrix
 
 py_logger = pylogger.get_pylogger(__name__)
 
@@ -87,26 +84,13 @@ class WandbPlottingCallback(WandbTrainingCallback):
         super().setup(trainer, pl_module, stage=stage)
 
         if not self.no_logger:
-            if self.tables is None:
-                plot_metrics = pl_module.train_plot_metrics
-                columns = ["epoch", *[k.replace("train/", "") for k in plot_metrics.keys()]]
-                self.tables = {
-                    "train": wandb.Table(columns=columns),
-                    "val": wandb.Table(columns=columns),
-                    "test": wandb.Table(columns=columns),
-                }
-
-            if self.num_figs is None:
-                plot_metrics = pl_module.train_plot_metrics
-                self.num_figs = len(plot_metrics)
-
             if hasattr(trainer.datamodule, "num_to_labels"):
                 self.num_to_labels = trainer.datamodule.num_to_labels
             else:
                 self.num_to_labels = None
 
     def on_epoch_end_plotting(self, trainer, pl_module, phase="train"):
-        if not self.no_logger and self.num_figs > 0:
+        if not self.no_logger:
             plot_metrics = getattr(pl_module, f"{phase}_plot_metrics")
             current_epoch = trainer.current_epoch
 
@@ -118,16 +102,11 @@ class WandbPlottingCallback(WandbTrainingCallback):
                     index = list(range(array.shape[0]))
                     if self.num_to_labels is not None:
                         index = [self.num_to_labels[i] for i in index]
-
                     df_cm = pd.DataFrame(array, index=index, columns=index)
                     df_cm.index.name = "Actual"
                     df_cm.columns.name = "Predicted"
-                    # df_cm_normed = array / array.sum(axis=1, keepdims=True)
-
                     fig_, ax_ = plt.subplots(**self.fig_kws)
                     sns.heatmap(df_cm, ax=ax_, **self.plot_kws)
-
-                    # fig_, ax_ = pp_matrix(df_cm, **self.kwargs)
                 else:
                     fig_, ax_ = metric.plot(**self.plot_kws)
                 metric.reset()
